d47fb51a0eec21679c16aa1345e5c66e584c974a
Alex <alexokabo2021@gmail.com>
2023-06-15 11:11:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <unistd.h
-
+#include <unistd.h>
 /**
 * main - A c program that prints a line to the standard error
 * Return 1 (Success)
@@ -10,4 +9,5 @@ int main(void)
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 return (1);
 }
+
 
