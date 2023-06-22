62fa6fc27f25cbe13c292af73c474be1944f0234
Alex <alexokabo2021@gmail.com>
2023-06-22 06:15:54 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Mulitiple two integers
+ * @x: The first number to multiply
+ * @y: The second numbe to multipy with the firdt number
+ * Return: The resul of multiplication between x and y
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
