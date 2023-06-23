b6c18e847e6c86ec26adeb900c21dbfd7d37bdf9
Alex <alexokabo2021@gmail.com>
2023-06-23 04:39:27 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,15 @@
 void more_numbers(void)
 {
 	int x, y;
+
 	for (x = 0; x < 10; x++)
 	{
 	for (y = 0; y <= 14; y++)
 	{
-	if (y > 9)
-	{
-	_putchar((y / 10) + '0');
-	}
+	if (y >= 9)
+	_putchar('1');
 	_putchar((y % 10) + '0');
 	}
-	putchar('\n');
+	_putchar('\n');
 	}
 }
