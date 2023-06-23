88b94d9eb8e1e11705d56ee4a9941ec5dfe556dd
Alex <alexokabo2021@gmail.com>
2023-06-23 06:49:01 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,16 +14,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i =  1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
-		else if ("Buzz")
-		else
-			print("%i", i);
-		if (i < 100)
+		else if (i % 5 == 0)
+			printf("%i", i);
+		else if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
