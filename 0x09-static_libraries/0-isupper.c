79f57fb3476c31935a27880212f4b09d1f9ff566
Alex <alexokabo2021@gmail.com>
2023-07-07 02:06:37 -0700

    main.h

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper.o  check if uppercase letter
+ * @n: char to check
+ *
+ * Return: if uppercase return 1, if not return 0
+ */
+int _isupper(int n)
+{
+	if (n > 'A' && n <= 'z')
+		return (1);
+
+	return (0);
+}
