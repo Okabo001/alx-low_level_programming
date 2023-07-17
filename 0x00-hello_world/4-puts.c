b0c7bc6836e7655c505d28d72342a08d16e3ea94
Alex Okabo <alexokabo202@gmail.com>
2023-07-17 16:56:52 +0300

    prototype of main.h

--- 0x00-hello_world/4-puts.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-/*main A program that prints a line with puts functions
- * Return : 0 (Success)
- */
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -wall -pendatic -werror -wextra -c *.c
-ar -rc liball.a *.c
-ranlib liball.a
